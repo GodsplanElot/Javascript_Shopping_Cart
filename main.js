44165e8c345ceb826b87a1f55204cb8968670483
GodsplanELot <elotgodsplan2@gmail.com>
2024-11-23 06:24:19 +0100

    Finished the logic for the shopping cart icon

--- main.js
+++ main.js
@@ -30,11 +30,12 @@ let shopitemsData = [{
 },
 ];
 
-let basket = []
+let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 let generateShop =()=>{
     return (shop.innerHTML =  shopitemsData.map((x) => {
         let { id, name, price, desc, img } = x;
+        let search = basket.find((x) => x.id === id) || [];
         return `
         <div id=product-id-${id} class="item">
         <img width="220" src="${img}" alt="">
@@ -45,7 +46,7 @@ let generateShop =()=>{
                 <h2>$ ${price}</h2>
                 <div class="buttons">
                     <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-                    <div id=${id} class="quantity">0</div>
+                    <div id=${id} class="quantity">${search.item === undefined? 0: search.item}</div>
                     <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
 
                 </div>
@@ -73,6 +74,7 @@ let increment = (id)=> {
         search.item += 1
     }
 
+    localStorage.setItem("data", JSON.stringify(basket));
    // console.log(basket);
     update(selectedItem.id)
 };
@@ -84,7 +86,7 @@ let decrement = (id)=> {
     else{
         search.item -= 1;
     }
-
+    localStorage.setItem("data", JSON.stringify(basket));
    // console.log(basket);
     update(selectedItem.id)
 };
@@ -97,6 +99,9 @@ let update = (id)=> {
 };
 
 let calculation =()=>{
-    let cartIcon = document.getElementById("cartAmount")
-    console
+    let cartIcon = document.getElementById("cartAmount");
+    cartIcon.innerHTML = basket.map((x)=>x.item).reduce((x,y)=>x+y,0);
+    
 }
+
+calculation();
