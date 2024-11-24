2da99b3f268179fba041fe02250efd83f490d350
GodsplanELot <elotgodsplan2@gmail.com>
2024-11-24 23:40:41 +0100

    updated the html and the css files

--- src/main.js
+++ src/main.js
@@ -1,34 +1,6 @@
 let shop = document.getElementById('shop');
 
-let shopitemsData = [{
-    id:"jaflam",
-    name:"casual Shirt",
-    price: 45,
-    desc: "lorem iosum dolor sit amet consetureer adipisicing",
-    img: "images/img-1.jpg"
-},
-{
-    id:"hsjyhsj",
-    name:"Office Shirt",
-    price: 100,
-    desc: "lorem iosum dolor sit amet consetureer adipisicing",
-    img: "images/img-2.jpg"
-},
-{
-    id:"lammafuck",
-    name:"T Shirt",
-    price: 25,
-    desc: "lorem iosum dolor sit amet consetureer adipisicing",
-    img: "images/img-3.jpg"
-},
-{
-    id:"cattuna",
-    name:"Mens Suit",
-    price: 300,
-    desc: "lorem iosum dolor sit amet consetureer adipisicing",
-    img: "images/img-4.jpg"
-},
-];
+
 
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
