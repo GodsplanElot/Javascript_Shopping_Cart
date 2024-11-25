f285d39aec37a600b4cc33826db7920417c3fd24
GodsplanELot <elotgodsplan2@gmail.com>
2024-11-25 16:59:00 +0100

    writing logic for cart page and styling it

--- src/cart.js
+++ src/cart.js
@@ -1,3 +1,5 @@
+let label = document.getElementById('label');
+let shoppingCart = document.getElementById('shopping-cart');
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 
@@ -8,3 +10,30 @@ let calculation =()=>{
 }
 
 calculation();
+
+let generateCartItems = ()=>{
+    if (basket.length !== 0) {
+        return (shoppingCart.innerHTML = basket.map((x) =>{
+            let { id, item } = x;
+            let search = shopitemsData.find((y)=>y.id === id) || [];
+            return `
+            <div class="cart-item">
+            <img width="100px" src=${search.img} alt="" />
+            <div class="details">
+            
+            </div>
+            </div>
+            `;
+        }).join(""));
+    } else {
+        shoppingCart.innerHTML = ``;
+        label.innerHTML = `
+        <h2>Cart is Empty</h2>
+        <a href="index.htm">
+        <button class= "HomeBtn"> Back to home </button>
+        </a>
+        `;
+    };
+};
+
+generateCartItems();
