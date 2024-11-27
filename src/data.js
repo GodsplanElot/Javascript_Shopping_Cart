05e862e2b6dfcbcbaefa18d8f24b145c8994a11b
GodsplanELot <elotgodsplan2@gmail.com>
2024-11-27 10:38:03 +0100

    completed the cart and all the logic, this is the final commit

--- src/data.js
+++ src/data.js
@@ -1,29 +1,31 @@
-let shopitemsData = [{
-    id:"jaflam",
-    name:"casual Shirt",
-    price: 45,
-    desc: "lorem iosum dolor sit amet consetureer adipisicing",
-    img: "images/img-1.jpg"
-},
-{
-    id:"hsjyhsj",
-    name:"Office Shirt",
-    price: 100,
-    desc: "lorem iosum dolor sit amet consetureer adipisicing",
-    img: "images/img-2.jpg"
-},
-{
-    id:"lammafuck",
-    name:"T Shirt",
-    price: 25,
-    desc: "lorem iosum dolor sit amet consetureer adipisicing",
-    img: "images/img-3.jpg"
-},
-{
-    id:"cattuna",
-    name:"Mens Suit",
-    price: 300,
-    desc: "lorem iosum dolor sit amet consetureer adipisicing",
-    img: "images/img-4.jpg"
-},
-];
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs",
+      name: "Casual Shirt",
+      price: 45,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-1.jpg",
+    },
+    {
+      id: "ioytrhndcv",
+      name: "Office Shirt",
+      price: 100,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-2.jpg",
+    },
+    {
+      id: "wuefbncxbsn",
+      name: "T Shirt",
+      price: 25,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-3.jpg",
+    },
+    {
+      id: "thyfhcbcv",
+      name: "Mens Suit",
+      price: 300,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-4.jpg",
+    },
+  ];
+  
